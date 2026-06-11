b889dc0f4b3079e4548a73bc4e640e26dba997c1
curiousdave1472 <davidshanlay@gmail.com>
2026-06-11 21:54:27 +0000

    While loops

--- main.js
+++ main.js
@@ -2,8 +2,24 @@
 var sum = 0;
 
 for (var i = 0; i < 5; i++) {
+
     sum += i;
+
     console.log("My current sum is: " + sum);
+
 }
 
 // Then write the while loop version below
+
+var sum = 0;
+
+var i = 0;
+
+while (i < 5) {
+
+  sum = sum + i;
+
+  console.log("My current sum is: " + sum);
+
+  i = i + 1;
+}
